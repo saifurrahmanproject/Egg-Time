b088137b4f7c8f2e3dc08e5a10079fc845337031
saifurrahmanproject <s.r.tonoy.p@gmail.com>
2020-10-08 23:15:24 -0400

    Add code for timer

--- Egg Time/ViewController.swift
+++ Egg Time/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var titleLevel: UILabel!
+    
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+  
+    @IBAction func eggSelected(_ sender: UIButton) {
+        
+       
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLevel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
-
-
+    
+    @objc func updateTimer() {
+        
+        if secondsPassed < totalTime {
+           secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+        } else {
+            
+            timer.invalidate()
+            titleLevel.text = "Done!"
+        }
+    }
+ 
 }
 
